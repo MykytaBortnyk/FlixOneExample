9b95fa76f15f855ba0a464ab5da03b3008ac621b
MykytaBortnyk <210401n@gmail.com>
2021-07-08 21:53:19 +0300

    basic Console UI implemented

---
+++ FlixOne/FlixOne.InventoryManagementClient/ConsoleUserInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlixOne.InventoryManagementClient
+{
+    public class ConsoleUserInterface
+    {
+        public string ReadValue(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        public void WriteMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+        public void WriteWarning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+        }
+    }
+}
