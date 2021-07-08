849dc6d937b4405d10c941368a9b1216659e74f6
MykytaBortnyk <210401n@gmail.com>
2021-07-08 17:21:45 +0300

    Tests has been updated

--- FlixOne/FlixOne.InventoryManagementTests/UnitTest1.cs
+++ FlixOne/FlixOne.InventoryManagementTests/UnitTest1.cs
@@ -9,5 +9,22 @@ namespace FlixOne.InventoryManagementTests
         public void TestMethod1()
         {
         }
+
+        [TestMethod]
+        private void AddInventoryCommand_Successful()
+        {
+            // создаем экземпл€р команды
+            // добавл€ем новую книгу с параметром "name"
+            // провер€ем, была ли добавлена книга с данным названием и количеством 0 
+            Assert.Inconclusive("AddInventoryCommand_Successful has not been implemented");
+        }
+        [TestMethod]
+        private void GetInventoryCommand_Successful() => Assert.Inconclusive("GetInventoryCommand_Successful has not been implemented");
+        [TestMethod]
+        private void HelpCommand_Successful() => Assert.Inconclusive("HelpCommand_Successful has not been implemented");
+        [TestMethod]
+        private void QuitCommand_Successful() => Assert.Inconclusive("QuitCommand_Successful has not been implemented");
+        [TestMethod]
+        private void UpdateQuantity_Successful() => Assert.Inconclusive("UpdateQuantity_Successful has not been implemented");
     }
 }
