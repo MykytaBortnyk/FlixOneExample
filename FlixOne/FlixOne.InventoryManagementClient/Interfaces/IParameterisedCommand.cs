2d4d15fe80955a82a9e9af50177bac3b620faccf
MykytaBortnyk <210401n@gmail.com>
2021-07-08 21:52:41 +0300

    IParameterisedCommand added

---
+++ FlixOne/FlixOne.InventoryManagementClient/Interfaces/IParameterisedCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlixOne.Interfaces.InventoryManagementClient
+{
+    public interface IParameterisedCommand
+    {
+        bool GetParameters();
+    }
+}
