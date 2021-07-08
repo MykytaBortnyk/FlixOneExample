a2a76b0b41d5d5c29e0fc76cb71962e98e17ee36
MykytaBortnyk <210401n@gmail.com>
2021-07-08 21:55:59 +0300

    AddInventoryCommand added

---
+++ FlixOne/FlixOne.InventoryManagementClient/AddInventoryCommand.cs
@@ -0,0 +1,29 @@
+﻿using FlixOne.Interfaces.InventoryManagementClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlixOne.InventoryManagementClient
+{
+    public class AddInventoryCommand : InventoryCommand, IParameterisedCommand
+    {
+        public string InventoryName { get; private set; }
+        /// <summary>
+        /// AddInventoryCommand requires name
+        /// </summary>
+        /// <returns></returns>
+        public bool GetParameters()
+        {
+            if (string.IsNullOrWhiteSpace(InventoryName))
+                InventoryName = GetParameter("name");
+            return !string.IsNullOrWhiteSpace(InventoryName);
+        }
+
+        internal override bool InternalCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FlixOne/FlixOne.InventoryManagementClient/InventoryCommand.cs
+++ FlixOne/FlixOne.InventoryManagementClient/InventoryCommand.cs
@@ -1,14 +1,27 @@
-﻿namespace FlixOne.InventoryManagementClient
+﻿using FlixOne.Interfaces.InventoryManagementClient;
+
+namespace FlixOne.InventoryManagementClient
 {
     public abstract class InventoryCommand
     {
         private readonly bool _isTerminatingCommand;
         internal InventoryCommand(bool commandIsTerminating) => _isTerminatingCommand = commandIsTerminating;
-        public bool RunCommand(out bool shouldQuit)
-        { 
-            shouldQuit = _isTerminatingCommand; 
-            return InternalCommand(); 
+        public (bool wasSuccessful, bool shouldQuit) RunCommand()
+        {
+            if (this is IParameterisedCommand parameterisedCommand)
+            {
+                var allParametersCompleted = false;
+                while (allParametersCompleted == false)
+                {
+                    allParametersCompleted = parameterisedCommand.GetParameters();
+                }
+            }
+            return (InternalCommand(), _isTerminatingCommand); 
         }
         internal abstract bool InternalCommand();
+    }
+    internal abstract class NonTerminatingCommand : InventoryCommand
+    {
+        protected NonTerminatingCommand() : base(commandIsTerminating: false) { }
     }
 }
