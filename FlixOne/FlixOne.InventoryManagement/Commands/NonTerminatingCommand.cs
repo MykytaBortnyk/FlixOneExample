7ddc8fdfe5d744ab551df0bca63cad55ee4e07de
MykytaBortnyk <210401n@gmail.com>
2021-07-10 14:34:40 +0300

    Moved to own file

---
+++ FlixOne/FlixOne.InventoryManagement/Commands/NonTerminatingCommand.cs
@@ -0,0 +1,15 @@
+﻿using FlixOne.InventoryManagement.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlixOne.InventoryManagement
+{
+    internal abstract class NonTerminatingCommand : InventoryCommand
+    {
+        protected NonTerminatingCommand(IUserInterface userInterface) : 
+            base(commandIsTerminating: false, userInteface: userInterface) { }
+    }
+}
